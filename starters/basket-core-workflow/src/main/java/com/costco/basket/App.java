a9c0d3abb97fe1ebdf11abf33cd906ba72833d06
Ankur Patel <ankurspatel@hotmail.com>
2024-10-29 10:35:40 -0700

    maven projects added

---
+++ starters/basket-core-workflow/src/main/java/com/costco/basket/App.java
@@ -0,0 +1,13 @@
+package com.costco.basket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
